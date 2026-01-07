e96422efe35df7010c4605c4eaa0ceadae2b66af
Wanshui Ma <wanshuim@nycstudents.net>
2026-01-07 11:04:48 -0500

    still bugged, working on script

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //hi 
+
+
+let scene;
+
+window.addEventListener("DOMContentLoaded",initializePage())
+
+function initializePage(){
+    scene = document.getElementsByTagName("a-scene");
+    const box = document.createElement("a-box");
+    box.setAttribute("scale",{x:2,y:2,z:2})
+    console.log(scene)
+    scene.append(box)
+}
