8506359fa7716f26a82ee3aed23bd13f31652e8e
Wanshui Ma <wanshuim@nycstudents.net>
2026-01-12 14:58:19 -0500

    Playing with different libraries

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 window.addEventListener("DOMContentLoaded",function (){
     const scene = document.querySelector("a-scene");
     const box = document.createElement("a-box");
-    box.setAttribute("color","black");
+    box.setAttribute("color","blue");
     box.setAttribute("dynamic-body","")
-    box.setAttribute("position","0 .5 0");
+    box.setAttribute("position","0 5.5 0");
     console.log(box)
     scene.append(box);
 } )
