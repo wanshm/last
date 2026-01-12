d0442b3e3042de50c035a7f4f4ccd8c9e69c7e34
Wanshui Ma <wanshuim@nycstudents.net>
2026-01-12 10:58:45 -0500

    implemented movement controls

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded",function (){
     const scene = document.querySelector("a-scene");
     const box = document.createElement("a-box");
     box.setAttribute("color","black");
+    box.setAttribute("dynamic-body","")
     box.setAttribute("position","0 .5 0");
     console.log(box)
     scene.append(box);
