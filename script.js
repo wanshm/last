1f5e8181b0726d7eac8fdc8995f0fc7705795dd4
Wanshui Ma <wanshuim@nycstudents.net>
2026-01-13 11:20:24 -0500

    cave prototype with terrain, look at a-frame-extras movement-controls constrain to nav mesh

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded",function (){
     const box = document.createElement("a-box");
     box.setAttribute("color","blue");
     box.setAttribute("dynamic-body","")
-    box.setAttribute("position","0 5.5 0");
+    box.setAttribute("position","0 25.5 0");
     console.log(box)
     scene.append(box);
 } )
