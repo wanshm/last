8fca1abc3accc139dceaa36d6037bc1c2cd5315d
Wanshui Ma <wanshuim@nycstudents.net>
2026-01-21 11:14:27 -0500

    started working on the basic slash attack animation
    Signed-off-by: Wanshui Ma <wanshuim@nycstudents.net>

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-
-
+let camera, scene
 window.addEventListener("DOMContentLoaded",function (){
-    const scene = document.querySelector("a-scene");
+    
+    camera = document.querySelector("#camerarig")
+    scene = document.querySelector("a-scene");
     const box = document.createElement("a-box");
     box.setAttribute("color","blue");
     box.setAttribute("dynamic-body","")
     box.setAttribute("position","0 25.5 0");
     console.log(box)
     scene.append(box);
+    
+
+    
+
 } )
 
+window.addEventListener("click",function(){
+    const scene = document.querySelector("a-scene");
+    const slash = document.createElement("a-ring");
+    
+    console.log(camera.object3D)
+
+    slash.setAttribute("side","double")
+    slash.setAttribute("rotation", {x:90,y:0,z:90})
+    console.log(slash)
+    slash.setAttribute("position",{ x:camera.object3D.position.x,y:camera.object3D.position.y +1,z:camera.object3D.position.z})
+    scene.append(slash)
+    setTimeout(() => {
+        slash.parentNode.removeChild(slash);
+    }, 1000);
+})
 
 
 
--- thing.js
+++
@@ -1,2 +0,0 @@
-//https://www.donmccurdy.com/2023/08/01/generating-gltf/
-// look at that later
