e51262d4a7df97a20333b652bb544cf3fdb5ed69
bw2789 <bojunw2789@nycstudents.net>
2026-01-22 15:52:10 +0000

    s

---
+++ Testing.js
@@ -0,0 +1,17 @@
+class Testing{
+    constructor(x,y,z){
+    this.obj = document.createElement("a-entity");
+  
+    let base = document.createElement("a-box");
+    base.setAttribute("color","red");
+    base.setAttribute("position","0 0.5 0");
+    base.setAttribute("height","0.1");
+    base.setAttribute("width","15");
+    base.setAttribute("depth","15");
+    this.obj.append( base );
+    
+    this.obj.setAttribute("position",{x:x, y:y, z:z});
+    scene.append( this.obj )
+  } 
+
+}
