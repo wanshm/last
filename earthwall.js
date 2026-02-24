b5a2af936f4a102128da05decfeccf7801902c30
Wanshui Ma <wanshuim@nycstudents.net>
2026-02-24 11:03:42 -0500

    ew

---
+++ earthwall.js
@@ -0,0 +1,11 @@
+class EarthWall{
+    constructor(){
+        
+        this.wall = document.createElement("a-box");
+
+        this.locator = document.createElement("a-ring");
+        const laser = document.createElement("a-cursor");
+
+        
+    }
+}
