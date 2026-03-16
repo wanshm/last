028c0fd0e391a5ea3aaee93789f5b1219870eb71
bw2789 <bojunw2789@nycstudents.net>
2026-03-16 15:05:51 +0000

    triagnel not int he reig plasce, making chooped bbuilding 1

--- ChoppedBuilding1.js
+++ ChoppedBuilding1.js
@@ -1,12 +1,62 @@
+class Building1ChoppedFloorBox{
+  constructor(x,y,z){
+    this.obj = document.createElement("a-box");
+    this.obj.setAttribute("position",{x:x, y:y, z:z});
+    this.obj.setAttribute("height","3");
+    this.obj.setAttribute("width","15");
+    this.obj.setAttribute("depth","15");
+    this.obj.setAttribute("force-pushable", "");
+    this.obj.setAttribute("side","double");
+    this.obj.setAttribute("static-body", " ");
+    scene.append( this.obj )
+  }
+}
+
+
+
 class ChoppedBuilding1{
     constructor(x,y,z){;
     
-    let base = new FloorBase(0,0,0);
-    base.obj.setAttribute("scale","1.5 1 1.5")
-    this.obj.append( base.obj );
+    let base1 = new FloorBase(0,0,0);
+    base1.obj.setAttribute("static-body", " ");
+    scene.append( base1.obj )
 
+
+    let floor1Box = new Building1ChoppedFloorBox(0,2,0);
+    scene.append( floor1Box.obj )
+
+    let base2 = new FloorBase(0 , 3.04 , 0);
+    scene.append( base2.obj );
     
-    this.obj.setAttribute("position",{x:x, y:y, z:z});
-    scene.append( this.obj )
+    let floor2Box = new Building1ChoppedFloorBox(0,5.1,0);
+    scene.append( floor2Box.obj )
+
+    let base3 = new FloorBase(0 , 6.1 , 0);
+    scene.append( base3.obj );
+
+    // let LeftRoof = new roof2(0,9.3,4.8);
+    // LeftRoof.obj.setAttribute("rotation",{x:0, y:90, z:0});
+    // scene.append( LeftRoof.obj );
+
+
+    // let RightRoof = new roof2(0,9.3,-4.8);
+    // RightRoof.obj.setAttribute("rotation",{x:0, y:-90, z:0});
+    // scene.append( RightRoof.obj );
+
+    let triangularRoof = document.createElement("a-triangle");
+    triangularRoof.setAttribute("material", "src", "Images/Wood.png");
+    triangularRoof.setAttribute("repeat", "5 5");
+    triangularRoof.setAttribute("position","-2.7 1.5 0");
+    triangularRoof.setAttribute("rotation",{x:0, y:0, z:90});
+    triangularRoof.setAttribute("side","double");
+    triangularRoof.setAttribute("vertex-a","3.3 0 0");
+    triangularRoof.setAttribute("vertex-b","-4.2 0 -7.5");
+    triangularRoof.setAttribute("vertex-c","-4.2 0 7.5");
+    triangularRoof.setAttribute("static-body", " ");
+    scene.append( triangularRoof );
+
+
+
+
 
 }}
